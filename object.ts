282a65e09ca6f4f856051ce5e44cecbdfbc3e89e
Dmitry <Dmitry>
2024-10-10 17:17:14 +0300

    add:file object.ts

---
+++ object.ts
@@ -0,0 +1,15 @@
+const informationAboutTheCar={
+    salon: "kombi",
+    securitysystems: "ABS",
+    pillows:"front",
+    assistanceSystems:"rain sensor",
+    exterior:"alloy wheels",
+    climate:"conditioner",
+    heating:"seats",
+    multimedia:"bluetooth",
+    comfort:"cruise control"
+}
+informationAboutTheCar.climate="climate control"
+
+ console.log(informationAboutTheCar)
+ 
